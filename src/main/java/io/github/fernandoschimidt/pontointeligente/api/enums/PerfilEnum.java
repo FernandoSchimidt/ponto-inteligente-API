eb0529d374dca8e93deb08a1b6cf88d789bfe0df
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2021-01-27 23:21:03 -0300

    api

---
+++ src/main/java/io/github/fernandoschimidt/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package io.github.fernandoschimidt.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/io/github/fernandoschimidt/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,16 @@
+package io.github.fernandoschimidt.pontointeligente.api.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.github.fernandoschimidt.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa,Long>{
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+	
+
+}
